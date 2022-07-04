74fd7845b982837e7d581f645cd49d385259c2b1
Love Tewari <92184403+lovetewari@users.noreply.github.com>
2022-07-04 23:23:37 +0530

    spin.py

---
+++ Spin_Game.py
@@ -0,0 +1,58 @@
+print("ENTER THE NUMBER YOU WANT TO INPUT THEN A NUMBER A RANDOM IS TAKEN AND THEN RESULT WE WE DECLATEWD THAT YOU WILL WON OR NOT")
+print("IF YOU WON YOU ARE GREAT")
+print("IF YOU FAIL/LOOSE TRY AGAIN")
+a=int(input("ENTER NUMBER(01)-"))
+b=int(input("ENTER NUMBER(02)-"))
+print("FINAL THINK DO YOU WANT REENTER OR NOT")
+print("IF YOU DONT WANT TO ENTER -'N' ")
+print("IF YOU WANT TO ENTER -'Y' ")
+c=input("ENTER 'Y/N'-")
+if c=='Y':
+    a=int(input("ENTER NO(01)-"))
+    b=int(input("ENTER NO(02)-"))
+    print("ITS YOY LAST TIME TO INPUT YOU HAD CHOOSES THAT THANK YOU!!!")
+    print("Lets Begin the Spin}}$$")
+    if a=='5' and b=='7':
+        print("You won!!")
+    else:
+        print("YOU LOOSE")
+if c=="N":
+    print("Thank you LETS begin the spin noW!!")
+    if a=='5' and b=='7':
+        print("You won!!")
+    else:
+        print("YOU LOOSE")
+print("Thank YOU for PLaying $$$")
+print("If you want to play again type='Y/N'")
+print("Its Your last chance to play")
+z=input("ENTER 'Y/N'")
+if z=='Y':
+      print("ENTER THE NUMBER YOU WANT TO INPUT THEN A NUMBER A RANDOM IS TAKEN AND THEN RESULT WE WE DECLATEWD THAT YOU WILL WON OR NOT")
+      print("IF YOU WON YOU ARE GREAT")
+      print("IF YOU FAIL/LOOSE TRY AGAIN")
+      a=int(input("ENTER NUMBER(01)-"))
+      b=int(input("ENTER NUMBER(02)-"))
+      print("FINAL THINK DO YOU WANT REENTER OR NOT")
+      print("IF YOU DONT WANT TO ENTER -'N' ")
+      print("IF YOU WANT TO ENTER -'Y' ")
+      c=input("ENTER 'Y/N'-")
+      if c=='Y':
+        a=int(input("ENTER NO(01)-"))
+        b=int(input("ENTER NO(02)-"))
+        print("ITS YOY LAST TIME TO INPUT YOU HAD CHOOSES THAT THANK YOU!!!")
+        print("Lets Begin the Spin}}$$")
+      if a=='5' and b=='7':
+        print("You won!!")
+      else:
+        print("YOU LOOSE")
+      if c=="N":
+        print("Thank you LETS begin the spin noW!!")
+      if a=='5' and b=='7':
+        print("You won!!")
+      else:
+        print("YOU LOOSE")
+        print("Thank YOU for PLaying $$$")
+else:
+      print("Thank You")
+      print("Hope To See You Again!!")
+      
